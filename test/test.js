3b99d245f2c892d5a5467c195df7f04cf0e049c4
cuihonglei <cuihonglei@baidu.com>
2018-03-01 21:17:34 +0800

    test npm

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var validator = require('validator-test');
+
+describe('Validator', function () {
+    describe('#isEmail', function () {
+        it('should return true when the string is an email address', function () {
+            if (validator.isEmail('foo@bar.net') !== true) {
+                throw new Erorr('Validator not right');
+            }
+        });
+    });
+});
